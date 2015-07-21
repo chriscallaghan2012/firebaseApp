0cf639de73e60060ec13e1d14554d921fd172eaf
gmorklla <gmorklla@gmail.com>
2015-07-21 15:51:54 -0500

    V.1.0.0
    Prueba no. 1

---
+++ www/js/app.js
@@ -0,0 +1,45 @@
+var myApp = angular.module('myApp', [
+	'ngRoute',
+	'firebase',
+	'controllers'
+	]);
+
+var controllers = angular.module('controllers', [
+	'firebase'
+	]);
+
+myApp.run(['$rootScope', '$location', function ($rootScope, $location) {
+	$rootScope.$on('$routeChangeError', function(event, next, previous, error){
+		if(error === 'AUTH_REQUIRED'){
+			$rootScope.mensaje = 'Debes iniciar sesión para tener acceso a ese contenido. ';
+			$location.path('/login');
+		}
+	});
+}]);
+
+myApp.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+	$routeProvider.
+		when('/login', {
+			templateUrl: 'views/login.html',
+			controller: 'ControlRegistro'
+		}). // route /login
+		when('/registro', {
+			templateUrl: 'views/registro.html',
+			controller: 'ControlRegistro'
+		}). // route /registro
+		when('/chat', {
+			templateUrl: 'views/chat.html',
+			controller: 'ControlChat',
+			resolve: {
+				currentAuth: function (Autenticacion) {
+					return Autenticacion.requiereAuth();
+				} // Autenticación requerida para entrar a /juntas
+			}
+		}). // route /juntas
+		otherwise({
+			redirectTo: '/login'
+		}); // route redirectTo /login
+		
+		//$locationProvider.html5Mode(true);
+	}
+]);
